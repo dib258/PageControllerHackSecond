73929742524747d483b7ae250fdb2a88af640330
dib258 <lesmanne@gmail.com>
2015-04-02 17:07:41 +0200

    First commit

---
+++ PageControllerHackSecondTryTests/PageControllerHackSecondTryTests.swift
@@ -0,0 +1,36 @@
+//
+//  PageControllerHackSecondTryTests.swift
+//  PageControllerHackSecondTryTests
+//
+//  Created by dib 258 on 01/04/15.
+//  Copyright (c) 2015 digitag. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PageControllerHackSecondTryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
